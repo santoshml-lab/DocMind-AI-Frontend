a50ae96082a430567b08e694fff1dbd5f6adb283
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-20 13:24:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import Home from "./pages/Home";
+import { useState } from "react";
+import UploadBox from "./UploadBox";
+import ChatBox from "./ChatBox";
 
 function App() {
-  return <Home />;
+  const [documentId, setDocumentId] = useState("");
+
+  return (
+    <>
+      <UploadBox onUploadSuccess={setDocumentId} />
+      <ChatBox documentId={documentId} />
+    </>
+  );
 }
 
 export default App;
